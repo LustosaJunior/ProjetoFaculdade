faedffac76ae64e210e266cec5b1d2187698fbdb
LustosaJunior <junior_dj_decio@hotmail.com>
2021-04-13 14:03:30 -0300

    Terminei Cockpit
    Coloquei o datatable com filtros na tabela

---
+++ JS/Cockpit.js
@@ -0,0 +1,19 @@
+$(document).ready( function () {
+    var table = $('#TableCVs').DataTable();
+
+    $('#txtNome').on( 'keyup', function () {
+        table.search( this.value ).draw();
+    } ); 
+
+    $('#txtArea').on( 'keyup', function () {
+        table.search( this.value ).draw();
+    } ); 
+
+    $('#txtUF').on( 'keyup', function () {
+        table.search( this.value ).draw();
+    } ); 
+
+    $('#txtCidade').on( 'keyup', function () {
+        table.search( this.value ).draw();
+    } ); 
+} );
